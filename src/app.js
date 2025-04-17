f2e025bebbedcc8711841ca5d189b84199acf1fa
Shivang Kumar <shivangkumar@Shivangs-MacBook-Air.local>
2025-04-17 11:08:04 +0530

    practited get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send("Hello from the server")
+// app.use('/test', (req, res) => {
+//     res.send("Hello from the test");
+// })
+
+app.get('/user', (req, res) => {
+    res.send({
+        firstName: "SHivang",
+        lastName: "Kumar"
+    })
 })
 
-app.use('/test', (req, res) => {
-    res.send("Hello from the test");
+app.post('/user', (req, res) => {
+    res.send("Data has added to DB successfully!")
+})
+
+app.delete('/user', (req, res) => {
+    res.send("Data has deleted sucessfully");
+})
+
+
+app.use('/', (req, res) => {
+    res.send("Hello from the server")
 })
 
 
